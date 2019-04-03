ae2b7f428af86535bede7f49393b4c734cc22aa9
byronqiji <byronqiji@hotmail.com>
2019-04-03 17:56:21 +0800

    x

--- Robot.Business/WeChat/State/Login.cs
+++ Robot.Business/WeChat/State/Login.cs
@@ -26,15 +26,19 @@ namespace Robot.Business.WeChat.State
         public override string Monitor()
         {
             //https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/synccheck?r=1498915088960&skey=%40crypt_4af0643b_9f615fbce9ddb2be3fce910f3e285ee8&sid=E5pvocibbKBAWjXh&
+            //https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/synccheck?r=1554282435522&skey=%40crypt_4af0643b_4cae09263ff05eaab459aa29f65399b1&sid=ebXHPrVwoOvZ5rlB&uin=1126851900&deviceid=e430041265548391&synckey=1_683279947%7C2_683279978%7C3_683279510%7C11_683279603%7C201_1554282384%7C203_1554276597%7C1000_1554280922%7C1001_1554280995&_=1554282339966
             //uin =1126851900&deviceid=e336493130571149&synckey=1_658954891%7C2_658955296%7C3_658955084%7C11_658955257%7C13_658700011%7C201_1498915083%7C203_1498913594%7C1000_1498903382%7C1001_1498903412&_=1498911897174
 
-            string url = $"https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/synccheck?r={userManager.User.RequestCount}&skey={HttpUtility.UrlEncode(userManager.User.SKey, Encoding.UTF8)}&sid={userManager.User.SID}&" +
+            //https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/synccheck?r=1554282435522&skey=%40crypt_4af0643b_4cae09263ff05eaab459aa29f65399b1&sid=ebXHPrVwoOvZ5rlB&uin=1126851900&deviceid=e430041265548391&synckey=1_683279947%7C2_683279978%7C3_683279510%7C11_683279603%7C201_1554282384%7C203_1554276597%7C1000_1554280922%7C1001_1554280995&_=1554282339966
+            string url = $"https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/synccheck?r={userManager.User.DateTimeDelt}&skey={HttpUtility.UrlEncode(userManager.User.SKey, Encoding.UTF8)}&sid={userManager.User.SID}&" +
                 $"uin={HttpUtility.UrlEncode(userManager.User.UIN, Encoding.UTF8)}&deviceid={userManager.User.DeviceID}&synckey={HttpUtility.UrlEncode(userManager.User.SyncKey, Encoding.UTF8)}&_={userManager.User.RequestCount}";
 
             string value = HttpHelper.GetResponseValue(url, userManager.User.Cookies);
 
             using (StreamWriter sw = new StreamWriter(Path + "\\sync.txt", true))
+            using (StreamWriter x = new StreamWriter(Path + "\\url.txt", true))
             {
+                x.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}: {url}");
                 sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "\t" + value);
             }
 
@@ -70,14 +74,17 @@ namespace Robot.Business.WeChat.State
                 {
                     sw.WriteLine(value);
                 }
-
-                //https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=QfLp+Z+FePzvOFoG&r=1377482079876
-                string u = $"https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid={userManager.User.SID}&r={userManager.User.RequestCount}";
+                //https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=ebXHPrVwoOvZ5rlB&skey=@crypt_4af0643b_4cae09263ff05eaab459aa29f65399b1&lang=zh_CN&pass_ticket=TL6offdixrMXke%252BaENCe2VJ%252BzirYrviM%252BPHkUnD7BlfgAuz1dEXez7dsP3UmVAGE
+                //https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=XVUX2odG2zc6R02i&skey=@crypt_4af0643b_78ac88387a52679170de6c7e0413f767&lang=zh_CN&pass_ticket=GvuWUeqTBjeVFiCgj0BIPM08%252FAt0v1n9b0cd4mb%252B9yxr%252F2rG7Bh5z%252Fja6A7vVKnV
+                //https://webpush.wx.qq.com   /cgi-bin/mmwebwx-bin/webwxsync?sid=QfLp+Z+FePzvOFoG&r=1377482079876
+                string u = $"https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid={userManager.User.SID}&skey={userManager.User.SKey}&lang=zh_CN&pass_ticket={userManager.User.PassTicket}";
                 
                 value = HttpHelper.GetResponseValue(u, HttpMethod.POST, jsonData);
 
-                using (StreamWriter sw= new StreamWriter(Path + "\\ + message.txt", true))
+                using (StreamWriter sw = new StreamWriter(Path + "\\ + message.txt", true))
+                using (StreamWriter x = new StreamWriter(Path + "\\url.txt", true))
                 {
+                    x.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}: {u}");
                     sw.WriteLine(value);
                 }
             }
--- Robot.Model/WeChat/UserInfo.cs
+++ Robot.Model/WeChat/UserInfo.cs
@@ -6,7 +6,7 @@ namespace Robot.Model.WeChat
 {
     public class UserInfo : IDisposable
     {
-        private static DateTime standardDateTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+        private static DateTime standardDateTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1, 8, 0, 0));
 
         int tip;
 
